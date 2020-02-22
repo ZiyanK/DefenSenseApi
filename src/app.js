e9c672045e2fbc62e3e50e63c00df6a3df31b934
ZiyanK <ziyankarmali786@gmail.com>
2020-02-22 17:21:52 +0530

    temp

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser")
 app.use(cors());
 const dotenv = require("dotenv");
 dotenv.config();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
